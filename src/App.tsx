66df0376fda7eb8a63b9e4d4ef31b5429efe0d8d
ulugbek <vulugb@gmail.com>
2025-01-18 12:23:24 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">kjnkjnk</header>
+    </div>
+  );
+}
+
+export default App;
