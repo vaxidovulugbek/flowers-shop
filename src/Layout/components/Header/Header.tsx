41653776af08cd23815765033c21f1310094061c
ulugbek <vulugb@gmail.com>
2025-02-07 20:45:00 +0500

    add logo

--- src/Layout/components/Header/Header.tsx
+++ src/Layout/components/Header/Header.tsx
@@ -1,10 +1,22 @@
 import React from "react";
 import "./styles.scss";
+import { NavLink } from "react-router-dom";
+import logo from "assets/imgs/bg.png";
 
 const Header: React.FC = () => {
 	return (
 		<>
-			<header className="header">header</header>
+			<header className="header">
+				<div className="container-box">
+					<NavLink to="/">
+						<img
+							style={{ width: "200px", height: "80px", objectFit: "cover" }}
+							src={logo}
+							alt="logo"
+						/>
+					</NavLink>
+				</div>
+			</header>
 		</>
 	);
 };
